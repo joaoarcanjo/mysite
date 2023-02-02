9ec4b5abaa66b4adde36fbdb6c21c5b170ab7aab
Arcanjo <joaopedro.arcanjo@hotmail.com>
2023-02-02 18:41:22 +0000

    Hobbies resize bug resolved.

--- src/screens/hobbies/Hobbies.tsx
+++ src/screens/hobbies/Hobbies.tsx
@@ -62,7 +62,7 @@ function HobbiesInfo() {
 export function Hobbies() {
     return (
         <div className="h-screen">
-        <div className="space-y-5 grid place-items-center py-5 px-10">
+        <div className="space-y-5 grid place-items-center py-5 px-2">
             <div className="max-w-md laptop:max-w-7xl grid place-items-center">
                 <MainInfo/>
                 <HobbiesInfo/>
